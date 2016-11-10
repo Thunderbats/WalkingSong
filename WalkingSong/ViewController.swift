c8effdfcdeed0a53df62e99df42c3550924db48a
Puntarikas <puntarikas@gmail.com>
2016-11-10 12:56:50 +0700

    first commit

---
+++ WalkingSong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WalkingSong
+//
+//  Created by Puntarika Susuntitapong on 11/10/2559 BE.
+//  Copyright © 2559 Trevi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
